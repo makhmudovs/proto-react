3fc7635c0f8af1fbd130d710be2ef0bd88f7df8d
makhmudovs <57782270+makhmudovs@users.noreply.github.com>
2020-05-28 15:35:30 +0800

    Updates

---
+++ precache-manifest.00a2a534b8487bf9d6a89b944b701f97.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a337731b7d828ec622a49b13767c0f",
+    "url": "/makhmudovs/proto-react.git/index.html"
+  },
+  {
+    "revision": "d3331a878c3a723dc5cb",
+    "url": "/makhmudovs/proto-react.git/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7ec423ec669294ae0dca",
+    "url": "/makhmudovs/proto-react.git/static/css/main.e19b8968.chunk.css"
+  },
+  {
+    "revision": "d3331a878c3a723dc5cb",
+    "url": "/makhmudovs/proto-react.git/static/js/2.2db8ac03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/makhmudovs/proto-react.git/static/js/2.2db8ac03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec423ec669294ae0dca",
+    "url": "/makhmudovs/proto-react.git/static/js/main.49ca8505.chunk.js"
+  },
+  {
+    "revision": "f43be24dd482c3ce8499",
+    "url": "/makhmudovs/proto-react.git/static/js/runtime-main.8e0a1859.js"
+  },
+  {
+    "revision": "2904299323454d895c81f6cb378c718b",
+    "url": "/makhmudovs/proto-react.git/static/media/01-icon-transport-700x700.29042993.png"
+  },
+  {
+    "revision": "cc13ec7a3e2bc98e8daf845d56488d55",
+    "url": "/makhmudovs/proto-react.git/static/media/02-icon-food-700x700.cc13ec7a.png"
+  },
+  {
+    "revision": "278a28361e8696a8b84c4ae5afc4c246",
+    "url": "/makhmudovs/proto-react.git/static/media/03-icon-delivery-700x700.278a2836.png"
+  },
+  {
+    "revision": "cf9e01586858aa51b43e4049747f5475",
+    "url": "/makhmudovs/proto-react.git/static/media/05-icon-airtime-700x700.cf9e0158.png"
+  },
+  {
+    "revision": "5d905808d2caa8d9465a54bb3e106286",
+    "url": "/makhmudovs/proto-react.git/static/media/11-icon-rewards-700x700.5d905808.png"
+  },
+  {
+    "revision": "52ff974e2bc506da53a126928a4a5f3a",
+    "url": "/makhmudovs/proto-react.git/static/media/15-icon-pay-700x700.52ff974e.png"
+  },
+  {
+    "revision": "162a2cce69bcc6df6c59120f3c593bce",
+    "url": "/makhmudovs/proto-react.git/static/media/DesktopContent1.162a2cce.png"
+  },
+  {
+    "revision": "0a1e359e3fdb3cc4e485ccdf54169b8a",
+    "url": "/makhmudovs/proto-react.git/static/media/DesktopContent3.0a1e359e.png"
+  },
+  {
+    "revision": "25504e7e19028977db0052c833b897e7",
+    "url": "/makhmudovs/proto-react.git/static/media/GetStarted-SignUp.25504e7e.png"
+  },
+  {
+    "revision": "ceb42576452ee178799166ab9c5087f1",
+    "url": "/makhmudovs/proto-react.git/static/media/Unknown-8.ceb42576.jpeg"
+  },
+  {
+    "revision": "9e9952c2d27635622497df63beeda89c",
+    "url": "/makhmudovs/proto-react.git/static/media/WhyBe-1.9e9952c2.png"
+  },
+  {
+    "revision": "ad3aa934202b9a8f3424fe12fc498be2",
+    "url": "/makhmudovs/proto-react.git/static/media/WhyBe-2-1.ad3aa934.png"
+  },
+  {
+    "revision": "301ba2abd091c079dbaa387c8a029bd5",
+    "url": "/makhmudovs/proto-react.git/static/media/bills-700x700.301ba2ab.png"
+  },
+  {
+    "revision": "a291039467c0cb1ad2163dd81116628d",
+    "url": "/makhmudovs/proto-react.git/static/media/clock-1.a2910394.png"
+  },
+  {
+    "revision": "e138c88f55fd51620176b83496dd4386",
+    "url": "/makhmudovs/proto-react.git/static/media/covid-delivery-partner-icon.e138c88f.png"
+  },
+  {
+    "revision": "82a39306a748a5b4796a891735cce45a",
+    "url": "/makhmudovs/proto-react.git/static/media/covid-image-delivery-services.82a39306.png"
+  },
+  {
+    "revision": "1b0a6ae0753df99b43ac45c916233ac2",
+    "url": "/makhmudovs/proto-react.git/static/media/covid-image-food-delivery-new.1b0a6ae0.png"
+  },
+  {
+    "revision": "18422f1015bbb05146e8ec59fc9f0f8e",
+    "url": "/makhmudovs/proto-react.git/static/media/covid-merchant-partner-icon.18422f10.png"
+  },
+  {
+    "revision": "eea47f957d7872d6fd873caa3adf7260",
+    "url": "/makhmudovs/proto-react.git/static/media/dg-grab-back.eea47f95.jpg"
+  },
+  {
+    "revision": "546c0e8da3ebca93ab3b81a953040a41",
+    "url": "/makhmudovs/proto-react.git/static/media/grabcar.546c0e8d.png"
+  },
+  {
+    "revision": "96b47f0ba9be4807135eb6e3a8b98325",
+    "url": "/makhmudovs/proto-react.git/static/media/icon-certainty-1.96b47f0b.png"
+  },
+  {
+    "revision": "c99301e2682e29eefbcdb82216809623",
+    "url": "/makhmudovs/proto-react.git/static/media/icon-grabgift-700x700.c99301e2.png"
+  },
+  {
+    "revision": "b7e27149d674e7c0b2a091913291a32c",
+    "url": "/makhmudovs/proto-react.git/static/media/icon-safety-3.b7e27149.png"
+  },
+  {
+    "revision": "1bc31480a4c1c69484990a6bcd5fe242",
+    "url": "/makhmudovs/proto-react.git/static/media/icon-speed-1.1bc31480.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/makhmudovs/proto-react.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1ff0f755e50195d24f5eb4618a317202",
+    "url": "/makhmudovs/proto-react.git/static/media/play-store-btn.1ff0f755.png"
+  },
+  {
+    "revision": "815c7426cf11bda634734f01be336cb3",
+    "url": "/makhmudovs/proto-react.git/static/media/protoPayIcon2.815c7426.png"
+  }
+]);
